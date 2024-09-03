9a58abeb1f96b25a6231a069ccaeb5489a36a3be
EagleToenail <eagletoenail111@gmail.com>
2024-09-03 14:14:28 -0400

    cors

--- src/helpers/socket.js
+++ src/helpers/socket.js
@@ -2,5 +2,5 @@ import { io } from 'socket.io-client';
 
 const isDev = process.env.NODE_ENV === 'development';
 
-const socket = io(isDev ? 'ws://localhost:8081' : '/');
+const socket = io(isDev ? 'ws://3.14.245.186:8081' : '/');
 export default socket;
